b988a2dc14f4d3338d3ad2365f0b63b32bbe8927
Hari Krishnan <photolens.codes.106@gmail.com>
2025-03-12 10:34:08 +0530

    Update rest.js

--- rest.js
+++ rest.js
@@ -69,7 +69,38 @@ app.post('/api/books', (req, res) => {
     res.status(201).json(newBook);
 });
 
+// PUT - Update an existing book by id
+app.put('/api/books/:id', (req, res)=> {
+    const id = parseInt(req.params.id);
+    const {title, author} = req.body;
+    const bookIndex = books.findIndex(book => book.id === id);
 
+    if (bookIndex === -1){
+        return res.status(404).json({message: "Maataan Vendi Nee Indaayitt Vende daaa"})
+    }
+
+    books[bookIndex] = {
+        ...books[bookIndex],
+        title: title || books[bookIndex].title,
+        author: author || books[bookIndex].author,
+    }
+
+    res.json(books[bookIndex]);
+})
+
+// DELETE - Remove a book by ID
+app.delete('/api/books/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const bookIndex = books.findIndex(book => book.id === id);
+
+    if (bookIndex === -1){
+        return res.status(404).json({message: "Ayin Thaakoolu ente kayill illallo!!!"})
+    }
+    // Removes one book at bookIndex from the books array
+    const deletedBook = books.splice(bookIndex, 1);
+    // It returns an array containing the deleted book (deletedBook[0]) is the acutal book object
+    res.json({message: "Avan Illandaayi", book: deletedBook[0]})
+})
 
 
 // Server Listening
