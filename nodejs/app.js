65659b1659a5408e5b46d856f4cfa732d06110ed
Hari Krishnan <photolens.codes.106@gmail.com>
2025-03-17 20:10:20 +0530

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,1 +1,21 @@
+const express = require('express')
+const PORT = 3000;
+const data = require('./data');
+const app = express();
+// Routing
+app.get('/', function(req, res, next){
+    const filters = req.query;
+    const filteredUsers = data.filter(user => {
+        let isValid = true;
+        for (key in filters){
+            console.log(key, user[key], filters[key]);
+            isValid = isValid && user[key]== filters[key]
+        }
+        return isValid;
+    });
+});
 
+// Server Listening
+app.listen(PORT, () => {
+    console.log(`Server Listening to http://localhost:3000?city=Metropolis​`);
+});
